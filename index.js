6ee1e9f2267924b61b1b766b57d317e8cf41b4fa
Zein19 <zein.23@hotmail.co.uk>
2023-07-21 20:09:25 +0100

    added max score and result

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
 let hScore = 0
 let gScore = 0
+const winningScore = 21
 
 const homeScore = document.getElementById(`home-score`)
 
 const guestScore = document.getElementById(`guest-score`)
 
+const result = document.getElementById('result')
+
+function isGameOver(){
+    if (hScore >= winningScore){
+    result.textContent="Winner: Home"
+}else if (gScore >= winningScore){
+    result.textContent="Winner: Guest"
+}}
+
 
 function addOneHome(){
-    hScore+=1
+    if(hScore<winningScore && gScore < winningScore){
+        hScore+=1
     homeScore.textContent=hScore
-}
+    isGameOver()
+}}
 
 function addTwoHome(){
-    hScore+=2
+    if(hScore<winningScore && gScore < winningScore){
+        hScore+=2
     homeScore.textContent=hScore
-}
+    isGameOver()
+}}
 
 function addThreeHome(){
-    hScore+=3
+    if(hScore<winningScore && gScore < winningScore){
+        hScore+=3
     homeScore.textContent=hScore
-}
+    isGameOver()
+}}
 
 function addOneGuest(){
-    gScore+=1
+    if(hScore<winningScore && gScore < winningScore){
+        gScore+=1
     guestScore.textContent=gScore
-}
+    isGameOver()
+}}
 
 function addTwoGuest(){
-    gScore+=2
+    if(hScore<winningScore && gScore < winningScore){
+        gScore+=2
     guestScore.textContent=gScore
-}
+    isGameOver()
+}}
 
 function addThreeGuest(){
-    gScore+=3
+    if(hScore<winningScore && gScore < winningScore){
+        gScore+=3
     guestScore.textContent=gScore
-}
+    isGameOver()
+}}
 
 function reset(){
     hScore = 0 
     gScore = 0
     homeScore.textContent=hScore
     guestScore.textContent=gScore
+    result.textContent=""
 }
+
